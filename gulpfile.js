c0db9eaa928189241ca33993e5ee75a7a93664a6
Samuel DIDIER-LAURENT <istuffs@gmail.com>
2019-02-05 15:27:25 +0100

    build(gulp): fix gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -44,7 +44,7 @@ function htmlTask() {
 };
 
 function jsTask() {
-  return gulp.src('./src/js/*.js')
+  return gulp.src('./src/js/**/*.js')
   .pipe(plumber({errorHandler: notify.onError("Error: <%= error.message %>")}))
   .pipe(babel({ presets: ['@babel/preset-env'] }))
   .pipe(gulpif(argv.production, uglify()))
@@ -53,24 +53,27 @@ function jsTask() {
 };
 
 function imgTask() {
-  return gulp.src('src/img/*.{gif,jpg,png,svg,jpeg}')
+  return gulp.src('src/img/**/*.{gif,jpg,png,svg,jpeg}')
     .pipe(imagemin())
     .pipe(gulp.dest('dist/img'));
 };
 
 
-function refresh() {
+function refresh(done) {
   browserSync.init({
     server: {
       baseDir: "./dist/"
     }
   });
+  done();
 };
 
+gulp.task('build', gulp.series(cssTask, jsTask, htmlTask, imgTask));
+
 /* default task and watch */
-gulp.task('watch', gulp.series(cssTask, jsTask, htmlTask, refresh, imgTask, function () {
+gulp.task('watch', gulp.series( 'build', refresh, function () {
   gulp.watch('./src/sass/**/*.{sass,scss}', gulp.series(cssTask));
-  gulp.watch('./src/js/*.js', gulp.series(jsTask));
+  gulp.watch('./src/js/**/*.js', gulp.series(jsTask));
   gulp.watch('./src/*.html', gulp.series(htmlTask));
   gulp.watch('./dist/*.html').on('change', browserSync.reload);
   gulp.watch('./dist/css/*.css').on('change', browserSync.reload);
